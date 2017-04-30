d7fc0af178eb206568e14f54a5b5a64b41fcec12
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2017-04-30 07:15:42 -0400

    Setup for heroku

--- H2O-API/server/db/mongoose.js
+++ H2O-API/server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/WaterApp");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/WaterApp");
 
-module.exports = { mongoose: mongoose };
+module.exports = {mongoose};
